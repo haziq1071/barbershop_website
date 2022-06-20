cf4c4e60d835f12cb523dba78c336afc6f315558
ICEMAN5057 <izzue5057@gmail.com>
2022-06-20 18:19:08 +0800

    try staffrole

--- src/main/java/com/example/suraubookingsystem/StaffServlet.java
+++ src/main/java/com/example/suraubookingsystem/StaffServlet.java
@@ -106,7 +106,7 @@ public class StaffServlet extends HttpServlet {
         try {
 
             Class.forName("org.postgresql.Driver"); // ni stay
-            String dbURL = "jdbc:postgresql://ec2-52-72-56-59.compute-1.amazonaws.com"; //ni url dri heroku database
+            String dbURL = "jdbc:postgresql://ec2-52-72-56-59.compute-1.amazonaws.com:5432/d274lnoegak379"; //ni url dri heroku database
             String user = "dnzxqagexabepj"; //ni user dri heroku database
             String pass = "edb330e6fe55ed3bb6d1ee1eb3c1f995e6b205eb5d464bee634abc3345b2d294"; //ni password dri heroku database
             Connection conn = DriverManager.getConnection(dbURL, user, pass);
