136aaa6f6cefc1f7372762aec67ffd0670b7476c
ICEMAN5057 <izzue5057@gmail.com>
2022-07-17 00:14:04 +0800

    test

--- src/main/java/com/example/suraubookingsystem/BookingServlet.java
+++ src/main/java/com/example/suraubookingsystem/BookingServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-
+//
 @MultipartConfig
 @WebServlet(name = "BookingServlet", value = "/BookingServlet")
 public class BookingServlet extends HttpServlet {
