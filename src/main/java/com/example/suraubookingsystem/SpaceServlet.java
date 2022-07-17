ab9dee1f75c002d1592ecac298ecb2f16aabcc53
ICEMAN5057 <izzue5057@gmail.com>
2022-07-18 04:30:58 +0800

    test

--- src/main/java/com/example/suraubookingsystem/SpaceServlet.java
+++ src/main/java/com/example/suraubookingsystem/SpaceServlet.java
@@ -180,7 +180,7 @@ public class SpaceServlet extends HttpServlet {
 
         private void deleteRoom(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
             int spaceid = Integer.parseInt(request.getParameter("spaceid"));
-            sp.deleteSpace(spaceid);
+            rm.deleteRoom(spaceid);
             response.sendRedirect("staffViewRoom.jsp");
         }
     }
